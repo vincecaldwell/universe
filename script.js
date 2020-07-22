3c4baed3ae47c7c7f897c5d0cc08a7a8a008679b
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-22 19:29:11 -0400

    updated cards to have length max, changed content slightly

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
-console.log("Hello World");
+function shortText() {
+var element = document.querySelectorAll('.card-text');
+var i;
+for(i=0; i < element.length; i++) {
+    // console.log(element[i].innerHTML);
+    var originalText = element[i].innerHTML;
+    var newText =originalText.substring(0,180);
+    element[i].innerHTML = newText + "...";
+    }
+}
+
+shortText();
